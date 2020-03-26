80e9023c0ef2ff3c7fef9668cb046b8b1a0a7727
danielbras <carloosdd9550@gmail.com>
2020-03-26 19:21:20 -0300

    added all web application screens

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,32 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn }  from 'react-icons/fi';
 
 import './styles.css';
 import '../../global.css';
 
 import logo from '../../assets/logo.svg';
-import herosImg from '../../assets/heroes.png';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
   return (
   <div className="logon-container">
     <section className="form">
-      <img src={logo} alt="Logon"/>
+      <img src={logo} alt="Be The Hero"/>
       
       <form>
         <h1>Faça seu Logon</h1>
 
         <input placeholder="Sua ID"/>
         <button className="button" type="submit" >Entrar</button>
-        <a href="/register">
+        <Link className="back-link" to="/register">
           <FiLogIn size={16} color="#e02041"/>
           Não tenho cadastro
-        </a>
-       
+        </Link>
       </form>
     </section>
 
-    <img src={herosImg} alt="Heros"/>
+    <img src={heroesImg} alt="Heroes"/>
   </div>
   )
 }
